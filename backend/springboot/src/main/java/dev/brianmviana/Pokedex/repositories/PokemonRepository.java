1b367172320db18292a2cddef852a40c3e282fcc
Brian M. Viana <brianmviana@gmail.com>
2022-02-11 14:22:34 -0300

    stating new spring project

--- backend/springboot/src/main/java/dev/brianmviana/Pokedex/repositories/PokemonRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.brianmviana.Pokedex.repositories;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import dev.brianmviana.Pokedex.models.Pokemon;
-
-public interface PokemonRepository extends ReactiveMongoRepository<Pokemon, String>{
-
-}
